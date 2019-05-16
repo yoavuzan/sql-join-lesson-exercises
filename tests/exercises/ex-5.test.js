30e7ad9719afee7432cbe3e8f26230a301949f82
unknown <omer.moradd@gmail.com>
2019-05-16 14:16:03 +0300

    Added ex5 tests

--- tests/exercises/ex-5.test.js
+++ tests/exercises/ex-5.test.js
@@ -1,28 +1,84 @@
 const SqlTestUtils = require('../sql_test_utils')
 
-describe("exercise5", () => {
-    const testUtils = new SqlTestUtils("Dolphin", "ex_5")
+describe("exercise2", () => {
+    const testUtils = new SqlTestUtils(["patient", "disease"], "ex_5", ["patient", "ethnicity", "gender", "disease", "symptoms"])
     afterEach(async (done) => {
         await testUtils.dropAndEndConnection()
         done()
     })
 
-    it('Should update any the "healthy" column of any dolphin for any brown or green dolphin to FALSE', async (done) => {
-        const isSelect = false
+    it('Should find all dolphins that have "on" anywhere in their name', async (done) => {
+        const isSelect = true
 
         await testUtils.createSQLConnection()
-        await testUtils.tableSetup([`
-        CREATE TABLE Dolphin(
-            name VARCHAR(20) NOT NULL PRIMARY KEY,
-            color VARCHAR(20),
-            height INT,
-            healthy BOOLEAN DEFAULT TRUE
-        )`,
-            `INSERT INTO Dolphin VALUES("d1", "blue", 0, DEFAULT);`,
-            `INSERT INTO Dolphin VALUES("d2", "green", 0, DEFAULT);`,
-            `INSERT INTO Dolphin VALUES("d3", "yellow", 0, DEFAULT);`,
-            `INSERT INTO Dolphin VALUES("d4", "brown", 0, DEFAULT);`,
-            `INSERT INTO Dolphin VALUES("d5", "red", 0, DEFAULT);`
+
+        await testUtils.tableSetup([
+            `
+            CREATE TABLE ethnicity (
+                id   INT NOT NULL PRIMARY KEY,
+                name VARCHAR(20)
+            );
+            CREATE TABLE gender (
+                id   INT NOT NULL PRIMARY KEY,
+                name VARCHAR(20)
+            );
+            CREATE TABLE symptoms (
+                family      INT NOT NULL PRIMARY KEY,
+                fever       BOOLEAN,
+                blue_whelts BOOLEAN,
+                low_bp      BOOLEAN
+            );
+            CREATE TABLE disease (
+                name          VARCHAR(20) NOT NULL PRIMARY KEY,
+                survival_rate FLOAT
+            );
+            CREATE TABLE patient(
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                ethnicity INT,
+                FOREIGN KEY(ethnicity) REFERENCES ethnicity(id),
+                gender INT,
+                FOREIGN KEY(gender) REFERENCES gender(id),
+                symptoms_family INT,
+                FOREIGN KEY(symptoms_family) REFERENCES symptoms(family),
+                disease VARCHAR(20),
+                FOREIGN KEY(disease) REFERENCES disease(name)
+            );
+
+            INSERT INTO ethnicity VALUES(0, "asian");
+            INSERT INTO ethnicity VALUES(1, "black");
+            INSERT INTO ethnicity VALUES(2, "hispanic");
+            INSERT INTO ethnicity VALUES(3, "white");
+
+            INSERT INTO gender VALUES(0, "female");
+            INSERT INTO gender VALUES(1, "male");
+
+            INSERT INTO symptoms VALUES(0, 1, 1, 1);
+            INSERT INTO symptoms VALUES(1, 0, 1, 1);
+            INSERT INTO symptoms VALUES(2, 0, 0, 1);
+            INSERT INTO symptoms VALUES(3, 0, 0, 0);
+            INSERT INTO symptoms VALUES(4, 1, 0, 1);
+            INSERT INTO symptoms VALUES(5, 1, 0, 0);
+            INSERT INTO symptoms VALUES(6, 1, 1, 0);
+            INSERT INTO symptoms VALUES(7, 0, 1, 0);
+
+            INSERT INTO disease VALUES("cabagge disease", 0.2);
+            INSERT INTO disease VALUES("lettuce disease", 0.35);
+
+            INSERT INTO patient VALUES(null, 0, 1, 7, 'cabagge disease');
+            INSERT INTO patient VALUES(null, 1, 0, 3, null);
+            INSERT INTO patient VALUES(null, 0, 0, 7, null);
+            INSERT INTO patient VALUES(null, 2, 1, 0, null);
+            INSERT INTO patient VALUES(null, 3, 1, 3, null);
+            INSERT INTO patient VALUES(null, 0, 1, 7, 'cabagge disease');
+            INSERT INTO patient VALUES(null, 2, 0, 0, null);
+            INSERT INTO patient VALUES(null, 1, 0, 4, 'cabagge disease');
+            INSERT INTO patient VALUES(null, 1, 1, 0, null);
+            INSERT INTO patient VALUES(null, 1, 0, 3, null);
+            INSERT INTO patient VALUES(null, 2, 0, 0, null);
+            INSERT INTO patient VALUES(null, 0, 1, 7, 'cabagge disease');
+            INSERT INTO patient VALUES(null, 0, 1, 0, 'lettuce disease');
+            INSERT INTO patient VALUES(null, 0, 0, 7, null);
+            INSERT INTO patient VALUES(null, 0, 1, 7, 'lettuce disease');`
         ])
 
         let studentQuery = await testUtils.getStudentQuery()
@@ -34,20 +90,11 @@ describe("exercise5", () => {
         expect(result.result, result.message).not.toBeNull()
         result = result.result
 
-        let healthyCount = result.filter(d => d.healthy === 1).length
-        expect(healthyCount, "Unexpected number of healthy dolphins! Only update the 'healthy' to FALSE for dolphins that are either brown OR green")
-            .toBe(3)
+        console.log( result )
 
-        let d2 = result.find(d => d.name === "d2")
-        let d4 = result.find(d => d.name === "d4")
-
-        for(let d of [d2, d4]){
-            expect(d, "Hmm, we're missing a dolphin. Make sure you're only updating, and not deleting anything by mistake")
-                .toBeTruthy()
-    
-            expect(d.healthy, `Found a ${d.color} dolphin tagged as healthy - should be false!`)
-                .toBeFalsy()
-        }
+        expect(result.length, "something").toBe(2)
+        expect(result[0]).toEqual({ symptoms_family: 4, count: 1 })
+        expect(result[1]).toEqual({ symptoms_family: 7, count: 3 })
 
         done() //for async
     });
